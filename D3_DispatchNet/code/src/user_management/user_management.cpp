346c6d2eff8041c634e326d932b3ac3dd190e6a9
le027885 <leonardo.tonetta@studenti.unitn.it>
2026-05-26 11:28:33 +0200

    Finished first version of user management classes

--- D3_DispatchNet/code/src/user_management/user_management.cpp
+++ D3_DispatchNet/code/src/user_management/user_management.cpp
@@ -1,36 +1,94 @@
 #include "user_management.h"
 #include <iostream>
 
-/*
+/** 
+* @brief Constructor for UserManagement class 
+*/
+UserManagement::UserManagement() {}
+
+/**
+ * @brief Destructor for UserManagement class
+ */
+UserManagement::~UserManagement() {}
+
+/**
+ * @brief Get the list of authenticated users in the system.
+ * @return A list of authenticated users.
+ */
+list<AuthenticatedUser> UserManagement::getAuthenticatedUsers() {
+    return authenticatedUsers;
+}
+
+/**
+ * @brief Get the list of anonymous users in the system.
+ * @return A list of anonymous users.
+ */
+list<AnonymousUser> UserManagement::getAnonymousUsers() {
+    return anonymousUsers;
+}
+
+/**
+ * @brief Add an authenticated user to the system.
+ * @param user The authenticated user to add.
+ */
+void UserManagement::addAuthenticatedUser(const AuthenticatedUser& user) {
+    authenticatedUsers.push_back(user);
+}
+
+/**
+ * @brief Add an anonymous user to the system, initialized with a unique 
+ * identifier.
+ */
+void UserManagement::addAnonymousUser() {
+    anonymousUsers.push_back(AnonymousUser(anonymousUsers.size() + 1));
+}
+
+/**
+ * @brief Remove an authenticated user from the system.
+ * @param user The authenticated user to remove.
+ */
+void UserManagement::removeAuthenticatedUser(const AuthenticatedUser& user) {
+    authenticatedUsers.remove(user);
+}
+
+/**
+ * @brief Remove an anonymous user from the system.
+ * @param user The anonymous user to remove.
+ */
+void UserManagement::removeAnonymousUser(const AnonymousUser& user) {
+    anonymousUsers.remove(user);
+}
+
+/**
 * @brief Destructor for AuthenticatedUser class
 */
 AuthenticatedUser::~AuthenticatedUser() {}
 
-/*
+/**
 * @brief Get the username of the authenticated user.
 * @return The username.
 */
 const string& AuthenticatedUser::getUsername() const { return username; }
 
-/*
+/**
 * @brief Get the email of the authenticated user.
 * @return The email.
 */
 const string& AuthenticatedUser::getEmail() const { return email; }
 
-/*
+/**
 * @brief Get the password of the authenticated user.
 * @return The password.
 */
 const string& AuthenticatedUser::getPassword() const { return password; }
 
-/*
+/**
 * @brief Get the type of the authenticated user.
 * @return The user type.
 */
 UserType AuthenticatedUser::getUserType() const { return userType; }
 
-/*
+/**
 * @brief Set the username of the authenticated user.
 * @param username The username to set.
 */
@@ -38,7 +96,7 @@ void AuthenticatedUser::setUsername(const string& username) {
     this->username = username;
 }
 
-/*
+/**
 * @brief Set the email of the authenticated user.
 * @param email The email to set.
 */
@@ -46,7 +104,7 @@ void AuthenticatedUser::setEmail(const string& email) {
     this->email = email;
 }
 
-/*
+/**
 * @brief Set the password of the authenticated user.
 * @param password The password to set.
 */
@@ -54,7 +112,7 @@ void AuthenticatedUser::setPassword(const string& password) {
     this->password = password;
 }
 
-/*
+/**
 * @brief Set the type of the authenticated user.
 * @param type The user type to set.
 */
@@ -62,14 +120,14 @@ void AuthenticatedUser::setUserType(UserType type) {
     this->userType = type;
 }
 
-/*
+/**
 * @brief Log out the authenticated user, moving to the anonymous user state.
 */
 void AuthenticatedUser::logout() {
     //TODO
 }
 
-/*
+/**
 * @brief Constructor for NetworkManager class
 * @param username The username of the network manager.
 * @param email The email of the network manager.
@@ -82,96 +140,123 @@ NetworkManager::NetworkManager(string username, string email, string password) {
     setUserType(UserType::NetworkManager);
 }
 
-/*
+/**
 * @brief Destructor for NetworkManager class
 */
 NetworkManager::~NetworkManager() {}
 
-/*
-* @brief Print the network, including stations, junctions, and lines.
+/**
+* @brief Destructor for EndUser class
 */
-void NetworkManager::viewNetwork() {
+EndUser::~EndUser() {}
+
+/**
+* @brief Print the data of the end user, that is username and email.
+*/
+void EndUser::viewData() {
+    cout << "Username: " << getUsername() << endl;
+    cout << "Email: " << getEmail() << endl;
+}
+
+/**
+* @brief Change the data of the end user.
+*/
+void EndUser::changeData() {
     //TODO
 }
 
-/*
-* @brief Create a new station in the network.
+/**
+* @brief Delete the account of the end user.
 */
-void NetworkManager::createStation() {
+void EndUser::deleteAccount() {
     //TODO
 }
 
-/*
-* @brief Create a new junction in the network.
+/**
+* @brief Constructor for TrainCompany class
+* @param username The username of the train company.
+* @param email The email of the train company.
+* @param password The password of the train company.
 */
-void NetworkManager::createJunction() {
+TrainCompany::TrainCompany(string username, string email, string password) {
+    setUsername(username);
+    setEmail(email);
+    setPassword(password);
+    setUserType(UserType::TrainCompany);
+}
+
+/**
+* @brief Destructor for TrainCompany class
+*/
+TrainCompany::~TrainCompany() {}
+
+/**
+* @brief Constructor for Passenger class
+* @param username The username of the passenger.
+* @param email The email of the passenger.
+* @param password The password of the passenger.
+*/
+Passenger::Passenger(string username, string email, string password) {
+    setUsername(username);
+    setEmail(email);
+    setPassword(password);
+    setUserType(UserType::Passenger);
+}
+
+/**
+* @brief Destructor for Passenger class
+*/
+Passenger::~Passenger() {}
+
+/**
+* @brief Get the list of tickets for the passenger.
+* @return A pointer to the list of tickets.
+*/
+list<Ticket>* Passenger::getTickets() const {
+    return const_cast<list<Ticket>*>(&tickets);
+}
+
+/**
+ * @brief Constructor for AnonymousUser class
+ * @param identifier The identifier for the anonymous user session.
+ */
+AnonymousUser::AnonymousUser(int identifier) : identifier(identifier) {}
+
+/**
+ * @brief Destructor for AnonymousUser class
+ */
+AnonymousUser::~AnonymousUser() {}
+
+/**
+ * @brief Get the identifier of the anonymous user session.
+ * @return The identifier.
+ */
+int AnonymousUser::getIdentifier() const {
+    return identifier;
+}
+
+/**
+ * @brief Register a new user account from the anonymous user state.
+ */
+void AnonymousUser::_register() {
     //TODO
 }
 
-/*
-* @brief Create a new line in the network.
-*/
-void NetworkManager::createLine() {
+/**
+ * @brief Reset the password of a user from the anonymous user state.
+ */
+void AnonymousUser::resetPassword() {
     //TODO
 }
 
-/*
-* @brief Edit an existing station in the network.
-* @param station The station to edit.
-*/
-void NetworkManager::editStation(Station*&) {
+/**
+ * @brief Log the anonymous user into the authenticated state.
+ */
+void AnonymousUser::login() {
     //TODO
 }
 
-/*
-* @brief Edit an existing junction in the network.
-* @param junction The junction to edit.
-*/
-void NetworkManager::editJunction(Junction*&) {
-    //TODO
-}
-
-/*
-* @brief Edit an existing line in the network.
-* @param line The line to edit.
-*/
-void NetworkManager::editLine(Line*&) {
-    //TODO
-}
-
-/*
-* @brief Delete an existing station from the network.
-* @param station The station to delete.
-*/
-void NetworkManager::deleteStation(Station*&) {
-    //TODO
-}
-
-/*
-* @brief Delete an existing junction from the network.
-* @param junction The junction to delete.
-*/
-void NetworkManager::deleteJunction(Junction*&) {
-    //TODO
-}
-
-/*
-* @brief Delete an existing line from the network.
-* @param line The line to delete.
-*/
-void NetworkManager::deleteLine(Line*&) {
-    //TODO
-}
-
-/*
-* @brief Review a service request submitted by a train company.
-* @param request The service request to review.
-*/
-void NetworkManager::reviewServiceRequest(ServiceRequest*&) {
-    //TODO
-}
-
-/*
+/**
 * @brief Overload the << operator for printing user types.
 * @param os The output stream.
 * @param type The user type to print.
