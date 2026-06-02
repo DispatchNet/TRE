902a5089801b24bb0c4bd1a7ec9be9233e6afe8c
lucaFiorini <luca.fio45@gmail.com>
2026-06-02 23:46:26 +0200

    Make network element storage map-based

--- D3_DispatchNet/code/src/infrastructure/Network.java
+++ D3_DispatchNet/code/src/infrastructure/Network.java
@@ -1,8 +1,10 @@
 package infrastructure;
 
-import java.util.List;
-import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
+import java.lang.IllegalArgumentException;
 import java.util.Collections;
+
 import service_mangement.ServiceSet;
 
 /**
@@ -11,53 +13,59 @@ import service_mangement.ServiceSet;
  * Provides an interface to add/remove elements from the network
  */
 public class Network {
-  List<Junction> junctions;
-  List<Line> lines;
-  List<ServiceSet> services;
+  Map<String,Junction> junctions;
+  Map<String,Line> lines;
+  Map<String,ServiceSet> services;
 
   public Network() {
-    this.junctions = new ArrayList<>();
-    this.lines = new ArrayList<>();
-    this.services = new ArrayList<>();
+    this.junctions = new HashMap<>();
+    this.lines = new HashMap<>();
   }
 
   /**
    * @brief Adds a Junction to the network
    * @param junction The junction to add
+   * @throws InvalidArgumentException The junction is already present in the map
    */
   public void addJunction(Junction junction) {
-    this.junctions.add(junction);
-  }
-
-  /**
-   * @brief Adds a Line to the network and updates relevant adjacencies
-   * @param j1 A Junction in the network
-   * @param j2 A Junction in the network
-   * @param lengthMeters The length of this line in meters
-   * @param maxSpeedKpH The max speed of this line in Kilometers per hour
-   * @param nTracks The number of tracks this line has
-   */
-  public void addLine(Junction j1, Junction j2, int lengthMeters, int maxSpeedKpH, int nTracks) {
-    this.lines.add(
-      new Line(
-        j1,j2,lengthMeters,maxSpeedKpH,nTracks
-      )
-    );
+    if (this.junctions.containsKey(junction.getId())){
+      throw new IllegalArgumentException(
+        String.format("cannot add Junction %s twice",junction.getId())
+      );
+    }
+    this.junctions.put(junction.getId(),junction);
   }
 
   /**
    * @brief Adds an existing Line object to the network.
    * @param line The line to add
+   * @throws IllegalArguemtneException if the line is already in the network
    */
   public void addLine(Line line) {
-    this.lines.add(line);
+    if (this.lines.containsKey(line.getId())){
+      throw new IllegalArgumentException(
+        String.format("cannote add Lien %s twice",line.getId())
+      );
+    }
+    this.lines.put(line.getId(),line);
   }
 
-  public List<Junction> getJunctions() {
-    return Collections.unmodifiableList(junctions);
+  /**
+   * @brief get the map of lines.
+   * @return An unmodifiable view of the lines
+   */
+  public Map<String,Line> getLines() {
+    return Collections.unmodifiableMap(lines);
   }
 
-  public List<Line> getLines() {
-    return Collections.unmodifiableList(lines);
+  /**
+   * @brief get the map of junctions.
+   * @return An unmodifiable view of the junctions
+   */
+  public Map<String,Junction> getJunctions() {
+    return Collections.unmodifiableMap(junctions);
   }
+
+  //TODO: Remove methods
+  
 }
