c424f0b0b8fe5b7e507adf194d17a9daf0460385
Alessio <alessio.zanon@unitn.it>
2026-06-10 14:30:12 +0200

    Just-in-case I delete a folder or two, Ticket and Path finding should be doxed

--- D3_DispatchNet/code/src/path_finding/Path_Finding.java
+++ D3_DispatchNet/code/src/path_finding/Path_Finding.java
@@ -12,7 +12,11 @@ import ticketing.Ticket;
 //TODO Temporary class, when properly implemented will be imported 
 
 public enum srtAlg {Length, Departure, Arrival, Cost};
-
+/**
+ * @class Path_finding
+ * @brief Class representing a path_finding request
+ * @details created whith all the relevant information, stores the search result and is able to sort and produce it multiple times
+ */
 class Path_Finding{
 	Passenger requester;
 	Junction from;
@@ -22,48 +26,105 @@ class Path_Finding{
 	srtAlg chosenSrt;
 	ArrayList<ArrayList<Ticket>> results;
 
-//Public	
-		public Path_Finding(Passenger req_, Junction from_, Junction to_, LocalTime aft_, LocalTime bef_) {
+  //Public	
+		/**
+     * @brief creates the Pathfinding_class
+     * @description Enforces mandatory information like requester and parameters of the search, initializes the sorting algorithm enum to its default value and computes the result lists
+     * @param requester which Passenger initiated the request, used for ticket generation
+     * @param from Where the search starts 
+     * @param to Where the search ends 
+     * @param after The earliest accepted time for the first train in a path to depart, is optional (all values pass) but if no before argument is specified defaults to current time
+     * @param before The latest accepted time for the last train in a path to arrive, is optional (all values pass)
+     * @exception NoPath The "from" Junction cannot reach the "to" Junction
+     * @exception BadSearch No path respects both "after" and "before" argument
+     * @see Passenger
+     * @see Junction
+     * @see Ticket
+     */
+    public Path_Finding(Passenger requester, Junction from, Junction to, LocalTime after, LocalTime before) {
 			//TODO
 		}
-
+    
+    /**
+     * @brief requester getter 
+     * @return a Passenger pointer
+     */
 		public Passenger getRequester() {
 			return requester;
 		}
-
+    
+    /**
+     * @brief from getter
+     * @return a Junction pointer
+     */
 		public Junction getFrom() {
 			return from;
 		}
 
+    /**
+     * @brief to getter
+     * @return a Junction pointer
+     */
 		public Junction getTo() {
 			return to;
 		}
-
+  
+    /**
+     * @brief after getter
+     * @return a LocalTime
+     */
 		public LocalTime getAfter() {
 			return after;
 		}
 
+    /**
+     * @brief before getter
+     * @return a LocalTime
+     */
 		public LocalTime getBefore() {
 			return before;
 		}
-
+    
+    /**
+     * @brief chosenSrt getter
+     * @reutrn a srtAlg enum
+     */
 		public srtAlg getChosenSrt() {
 			return chosenSrt;
 		}
-
+    
+    /**
+     * @brief chosenSrt setter
+     * @param newAlg new enum value to use 
+     */
 		public void setChosenSrt(srtAlg newAlg) {
 			chosenSrt = newAlg;
 		}
-
+    
+    /**
+     * @brief public way to access the results of the pathfinding 
+     * @description Calls the private srtPth function when necessary, and formats the results
+     */
 		public ArrayList<ArrayList<Ticket>> getResults() {
 			return srtPth();
 		}
-
+    
+    /**
+     * @brief Handles the pruchase of tickets from the POV of the pathfinder
+     * @param index which one of the lists of tickets is the one chosen by the user 
+     * @exception OutOfBounds The argument is not a valid index for the results list
+     */
 		public void prchTcks(int index) {
 		  //TODO
 		}
 
 //Private
+    /**
+     * @brief sorts the results list according to the stored srtAlg
+     * @description calls the complex comparator PathComparator to sort in the correct order
+     * @return A list of lists, of tickets
+     * @see PathComparator
+     */
 		ArrayList<ArrayList<Ticket>> srtPth() {
       Collections.sort(results, new PathComparator(chosenSrt));
 			return results;
@@ -74,6 +135,12 @@ class Path_Finding{
 		}
 };
 
+/**
+ * @class sortByLenght
+ * @brief The comparator that allows the paths to be sorted by sortByLenght
+ * @description Extracts time difference in a path and uses it as a primary sorting criterion, and uses junction count as a secondary one.
+ * @return A negative integer if the first argument is shorter, a positive integer otherwise. If equal, returns 0
+ */
 class sortByLenght implements Comparator<ArrayList<Ticket>> {
   public int compare (ArrayList<Ticket> L1, ArrayList<Ticket> L2) {
     //TODO Get difference of depature time of first and arrival of last for each list 
@@ -86,13 +153,24 @@ class sortByLenght implements Comparator<ArrayList<Ticket>> {
   }
 };
 
+/**
+ * @class sortByDeparture
+ * @brief The comparator that allows the paths to be sorted by departure time
+ * @description Extracts and compares the Departure information of the first train in each ticket list
+ * @return A negative integer if the first argument is after, a positive integer otherwise. If equal, returns 0
+ */
 class sortByDeparture implements Comparator<ArrayList<Ticket>> {
   public int compare (ArrayList<Ticket> L1, ArrayList<Ticket> L2) {
     //TODO compare departure times
     return 0;
   }
 };
-
+/**
+ * @class sortByArrival
+ * @brief The comparator that allows the paths to be sorted by arrival time
+ * @description Extracts and compares the arrival time from the lastStep information of the last ticket of each list
+ * @returns A negative integer if the first argument is earlier, a positive integer otherwise. If equal, returns 0
+ */
 class sortByArrival implements Comparator<ArrayList<Ticket>> {
   public int compare (ArrayList<Ticket> L1, ArrayList<Ticket> L2) {
     //TODO Compare arrival times
@@ -100,6 +178,12 @@ class sortByArrival implements Comparator<ArrayList<Ticket>> {
   }
 };
 
+/**
+ * @class sortByCost
+ * @brief The comparator that allows the paths to be sorted by Cost
+ * @description Extracts and compares the sum total of costs of each ticket list 
+ * @returns A negative integer if the first argument is cheaper, a postive integer otherwise. If equal, returns 0
+ */
 class sortByCost implements Comparator<ArrayList<Ticket>> {
   public int compare (ArrayList<Ticket> L1, ArrayList<Ticket> L2) {
     //TODO Compare Cost
@@ -107,10 +191,25 @@ class sortByCost implements Comparator<ArrayList<Ticket>> {
   }
 };
 
+/**
+ * @class PathComparator
+ * @brief Complex comparator that allows paths sorting according to a provided srtAlg parameter
+ * @description Compares by the selected system first, and if the result is indecisive attempts to use all others in order
+ * @returns A negative integer if the first argument is "better", a positive integer otherwise. If equal, returns 0
+ * @see sortByLenght
+ * @see sortByDeparture
+ * @see sortByArrival
+ * @see sortByCost
+ */
 class PathComparator implements Comparator<ArrayList<Ticket>> {
   private ArrayList<Comparator<ArrayList<Ticket>>> Comps;
   private srtAlg chosenSrt;
-
+  
+  /**
+   * @brief Comparator constructor 
+   * @description Initializes the sub-comparators, and sets the preferred one via the enum
+   * @param alg The preferred sorting enum
+   */
   public PathComparator (srtAlg alg) {
     this.Comps = new ArrayList<Comparator<ArrayList<Ticket>>>();
     this.Comps.add(new sortByLenght());
--- D3_DispatchNet/code/src/ticketing/Ticketing.java
+++ D3_DispatchNet/code/src/ticketing/Ticketing.java
@@ -10,29 +10,75 @@ import service_management.ServiceStep;
 import departure.Departure;
 import infrastructure.Junction;
 
+/**
+ * @class Ticket
+ * @brief Class representing a ticket, containing information about the owner, fist departure and last valid arrival
+ */
 public class Ticket{
   Passenger owner;
   Departure departure;
   ServiceStep lastStep;
 
-  //Public	
-    public Passenger getOwner() {
-      return owner
+  //Public
+  
+    /**
+     * @brief Constructor of Ticket class
+     * @description Constructs a ticket enforcing the mandatory owner, departure and step information
+     * @param owner The owner of the ticket
+     * @param departure The first station and train the ticket is valid from
+     * @param lastStep The last station and train the ticket is valid for 
+     * @see Departure
+     * @see ServiceStep
+     */
+    public Ticket (Passenger owner, Departure departure, ServiceStep lastStep) {
+      this.owner = owner;
+      this.departure = departure;
+      this.lastStep = lastStep;
     }
 
+    /**
+     * @brief Owner getter 
+     * @return a Passenger pointer
+     * @see Passenger
+     */
+    public Passenger getOwner() {
+      return owner;
+    }
+    
+    /**
+     * @brief Departure getter
+     * @return a Departure pointer
+     * @see Departure
+     */
     public Departure getDeparture() {
-      return departure
+      return departure;
     } 
 
+    /**
+     * @brief lastStep getter
+     * @return a ServiceStep pointer
+     * @see ServiceStep
+     */
     public ServiceStep getLastStep() {
-      return lastStep
+      return lastStep;
     }
 
+    /**
+     * @brief evalutes the total cost of the ticket
+     * @description uses the depature and lastStep information to measure the distance covered and which train is used, then computes the resulting price
+     * @return an integer representing the monetary value 
+     */
     public int getCost () {
       //TODO evaluate cost 
-      return 0
+      return 0;
     }
-
+    
+    /*
+     * @brief Get the last junction the train is valid for
+     * @description Looks into the lastStep value and extracts the junction it points total
+     * @return a Junction pointer
+     * @see Junction
+     */
     public Junction getJunction () {
       //TODO get junction
       return lastStep.getJunction();
@@ -41,6 +87,6 @@ public class Ticket{
 
   //Unit test
 		public static void main() {
+      
 		}
-};
-
+}
