a929ddb172e8eef35a9e99e093d1d35f71c3e252
le027885 <leonardo.tonetta@studenti.unitn.it>
2026-06-13 08:57:54 +0200

    fixed inconsistency in passenger

--- D3_DispatchNet/code/src/user_management/Passenger.java
+++ D3_DispatchNet/code/src/user_management/Passenger.java
@@ -5,8 +5,6 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import mail_service.Email;
-import payment_gateway.MockPaymentGateway;
-import payment_gateway.PaymentGateway;
 import ticketing.Ticket;
 
 /**
